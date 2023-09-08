83b6678a3ec9d1c43913f9fe1caa80ea7537a324
wesley price <wsprice13@gmail.com>
2023-09-07 23:10:50 -0500

    first push

---
+++ src/main/java/wesley/learningspringmaven/LearningSpring/LearningSpringApplication.java
@@ -0,0 +1,13 @@
+package wesley.learningspringmaven.LearningSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringApplication.class, args);
+	}
+
+}
---
+++ src/main/java/wesley/learningspringmaven/LearningSpring/domain/Book.java
@@ -0,0 +1,44 @@
+package domain;
+
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String isbn;
+
+    @ManyToMany
+    @JoinTable(name = "author_book", @JoinColumn(name = "book_id"), inverseJoinColumns = @JoinColumn(name = "author_id"))
+    private Set<Author> authors;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+}
